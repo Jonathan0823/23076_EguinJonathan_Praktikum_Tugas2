cf553c01a8f9574bcd09bff5126a8d00208675e6
Jonathan0823 <eguinjonathan08@gmail.com>
2024-08-30 15:20:11 +0700

    first commit

---
+++ src/Tugas1/MatematikaBeraksi.java
@@ -0,0 +1,17 @@
+package Tugas1;
+
+public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika mtk = new Matematika();
+        mtk.pertambahan(10, 5);
+        mtk.pengurangan(10, 5);
+        mtk.perkalian(10, 5);
+        mtk.pembagian(10, 5);
+
+        Matematika mtk1 = new Matematika();
+        mtk1.pertambahan(10, 5);
+        mtk1.pengurangan(10, 5);
+        mtk1.perkalian(10, 5);
+        mtk1.pembagian(10, 5);
+    }
+}
