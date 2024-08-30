c3c2254065625c5419e52d0f7181fcf9c40d8464
Jonathan0823 <eguinjonathan08@gmail.com>
2024-08-30 15:40:39 +0700

    Update to Value of mtk

--- src/Tugas1/MatematikaBeraksi.java
+++ src/Tugas1/MatematikaBeraksi.java
@@ -3,21 +3,9 @@ package Tugas1;
 public class MatematikaBeraksi {
     public static void main(String[] args) {
         Matematika mtk = new Matematika();
-        mtk.pertambahan(10, 5);
+        mtk.pertambahan(20, 10);
         mtk.pengurangan(10, 5);
-        mtk.perkalian(10, 5);
-        mtk.pembagian(10, 5);
-
-        Matematika mtk1 = new Matematika();
-        mtk1.pertambahan(10, 5);
-        mtk1.pengurangan(10, 5);
-        mtk1.perkalian(10, 5);
-        mtk1.pembagian(10, 5);
-
-        Matematika mtk2 = new Matematika();
-        mtk2.pertambahan(10, 5);
-        mtk2.pengurangan(10, 5);
-        mtk2.perkalian(10, 5);
-        mtk2.pembagian(10, 5);
+        mtk.perkalian(10, 3);
+        mtk.pembagian(21, 2);
     }
 }
